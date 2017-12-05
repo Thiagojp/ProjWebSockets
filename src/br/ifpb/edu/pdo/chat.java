27e27f791f182937090993753212579209ad326a
Thiagojp <ther_jp@hotmail.com>
2017-12-05 02:09:21 -0300

    Resolver bugs renomear

--- src/br/ifpb/edu/pdo/chat.java
+++ src/br/ifpb/edu/pdo/chat.java
@@ -72,31 +72,33 @@ public class chat {
 		} else if (message.toLowerCase().startsWith("rename")) {
 			String novonome = message.substring(message.indexOf(" "), message.length());
 			novonome=novonome.trim();
-			//System.out.println(novonome);
+			boolean exist = false;
 			for (Session s : Usuarios) {
-				/*if (s.getUserProperties().get("usuario").equals(novonome)) {
-					ses.getBasicRemote().sendText("Usuario: " + novonome + " ja existe na lista");
-					break;
-				} else*/ if (s.getUserProperties().get("usuario").equals(novonome) == false) {
-					//Usuarios.remove(ses);
+				if (s.getUserProperties().get("usuario").equals(novonome)==false) {
+					exist=true;		
 					ses.getUserProperties().put("usuario", novonome);
-					String username1 = (String) ses.getUserProperties().get("usuario");
-					System.out.println("Passou aqui" + novonome);
-											//ses.getUserProperties().put("usuario", novonome);
-						for (Session se : Usuarios) {
-							se.getBasicRemote().sendText("Usuario: " + username1 + " alterou o nome para "+novonome+"");
-						}
+					//String username1 = (String) ses.getUserProperties().get("usuario");
+					for (Session se : Usuarios) {
+					se.getBasicRemote().sendText("Usuario: " + username + " alterou o nome para "+novonome+"");										
 					
-				}
-			}
-		}
+					}					
+				} if(exist){
+					System.out.println("Nome quem recebe a msg"+username);										
+					}else{
+						ses.getBasicRemote().sendText("Usuario: " + novonome + " ja existe na lista");
+						
+						}
+					}	
 			
+		}
+	
 //MENSAGEM PRIVADA
 		 else if (message.toLowerCase().startsWith("send -user")) {
 			String[] msgArray = message.split(" ");
 			String nome = msgArray[2];
 			System.out.println(nome);
 			String mensagem = "";
+			boolean exist = false;// vamos assumir que nao existe
 			for (int i = 0; i < msgArray.length; i++) {
 				if (i > 2) {
 					mensagem = mensagem + " " + msgArray[i];
@@ -104,19 +106,22 @@ public class chat {
 			}
 
 			for (Session s : Usuarios) {
-				if (s.getUserProperties().get("usuario").toString().equals(nome)) {
-					s.getBasicRemote().sendText(
-							username + ":" + mensagem + " " + horas + "h" + minutos + " " + formatador.format(date));
-					
-				} /*else  {
-					if(s.getUserProperties().get("usuario").toString().contains(nome)== false){
-					System.out.println(nome);
-					s.getBasicRemote().sendText("Usuario: " + nome + " nao existe na lista");
-					break;
-					}
-				}*/
 
-			}
+				if (s.getUserProperties().get("usuario").toString().equals(nome)) {
+					exist =true;//existe
+					s.getBasicRemote().sendText(
+							username + ":" + mensagem + " " + horas + "h" + minutos + " " + formatador.format(date));	
+					break;//encontrou, nao precisa mais percorrer a lista
+					
+				}
+									
+				}			
+				if(exist){
+					System.out.println("Nome quem recebe a msg"+username);					
+					
+					}else{
+						ses.getBasicRemote().sendText("Usuario: " + nome + " nao existe na lista");
+					}			
 
 		} else {
 			ses.getBasicRemote().sendText("Comando Invalido");
